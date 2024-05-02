7b2b725b219ad2828b0d0806b852e6e952f45701
Susan GG <86454607+Suz4nGG@users.noreply.github.com>
2024-05-02 12:42:24 -0600

    endpint tierra

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,4 @@
-const BASE = 'http://10.0.1.7/WSSimulacionPrestamos/api/v1/simulacion'
+const BASE = 'http://10.0.0.73:8085/WSSimulacionPrestamos/api/v1/simulacion'
 
 export const ObtenerSimulacion = BASE + '/get-simulacion?idConsultante='
 export const ResponderSimulacion = BASE + '/aceptar-simulacion'
